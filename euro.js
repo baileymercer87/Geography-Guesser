c7d5d71e24e01057bd9d506d08f5279293fbce09
Tadpole87 <UP890294@myport.ac.uk>
2020-11-09 17:12:56 +0000

    Added basic Data and asking questions
    Added data in an array as i could not find a way of importing from a text file which I know would be alot better. Started asking questions but at the moment nothing more than that.

--- euro.js
+++ euro.js
@@ -1,6 +1,12 @@
 const keys = [];
 
 
+const countries = ["Liechtenstein","Albania","Andorra","Armenia","Austria","Azerbaijan","Belarus","Belgium","Bosnia + Herzegovina","Bulgaria","Croatia","Cyprus","Czechia","Denmark","Estonia","Finland","France","Georgia","Germany","Greece","Hungary","Iceland","Ireland","Isreal","Italy","Kazakhstan","Kyrgyzstan","Latvia","Lithuania","Luxembourg","Malta","Monaco","Montenegro","Netherlands","North Macedonia","Norway","Poland","Portugal","Republic of Moldova","Romania","Russia","San Marino","Serbia","Slovakia","Slovenia","Spain","Sweden","Switzerland","Tajikistan","Turkey","Turkmenistan","Ukraine","England","Uzebekistan","Wales","Scotland"];
+const cities =["Vaduz","Tirana","Andorra la vella","Yerevan","Vienna","Baku","Minsk","Brussels","Sarajevo","Sofia","Zagreb","Nicosia","Prague","Copenhagen","Tallinn","Helsinki","Paris","Tbilisi","Berlin","Athens","Budapest","Reykjavik","Dublin","Jerusalem","Rome","Nur-Sultan","Bishkek","Riga","Vilnius","Luxembourg City","Valletta","Monaco"," Podgorica","Amsterdam","Skopje","Oslo","Warsaw","Lisbon","Chisinau","Bucharest","Moscow","San Marino","Belgrade","Bratislava","Ljubljana","Madrid","Stockholm","Bern","Dushanbe","Ankara","Ashgabat","Kyiv","London","Tashkent","Cardiff","Edinbrugh£"];
+
+console.log(countries.length);
+console.log(cities.length);
+
 window.addEventListener('keydown', keysPressed);
 window.addEventListener('keyup', keysReleased);
 
@@ -21,5 +27,22 @@ function keysReleased(e) {
 
 
 function submit() {
-    console.log(document.getElementById('answerForm').value);
+    console.log(document.getElementById('question').innerHTML);
+    const pick = Math.floor(Math.random() * 2);
+    console.log(pick);
+    const point = Math.floor(Math.random() * 56);
+    console.log(point);
+    if (pick == 0) {
+        console.log("1");
+        document.getElementById("question").innerHTML = "Q - What is the capital of " + countries[point] + "?";
+    }
+    else {
+        console.log("2");
+        document.getElementById("question").innerHTML = "Q - " + cities[point] + " is the capital of what Country?";
+    }
+}
+
+function start() {
+    var elem = document.getElementById('beginButton');
+    elem.parentNode.removeChild(elem);
 }
